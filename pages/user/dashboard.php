a6c4061c86dd5e6c11fa339bcd89efa0da2b05c9
rakha-python <raditrakawar1@gmail.com>
2025-10-07 15:23:27 +0800

    nambahh dashboard user

--- pages/user/dashboard.php
+++ pages/user/dashboard.php
@@ -6,6 +6,13 @@ if ($_SESSION['role'] !== 'siswa') {
     header("Location: /login.php");
     exit;
 }
+
+
+$sql =  "SELECT * FROM projects WHERE user_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $user_id);
+$stmt->execute();
+$result = $stmt->get_result();
 ?>
 <!DOCTYPE html>
 <html lang="id">
@@ -17,6 +24,31 @@ if ($_SESSION['role'] !== 'siswa') {
     <h1>Selamat Datang, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
     <p>Ini adalah halaman dashboard siswa.</p>
 
+
+    <h2>Daftar Proyek Kamu</h2>
+
+    <?php if ($result->num_rows>0):?>
+        <table border="1" cellpadding="8">
+            <tr>
+                <th>Judul</th>
+                <th>Deskripsi</th>
+                <th>Deadline</th>
+                <th>Status</th>
+    </tr>
+    <?php while ($row = $result->fetch_assoc()):?>
+        <tr>
+            <td><?= htmlspecialchars($row['judul'])?></td>
+            <td><?= htmlspecialchars($row['deskripsi'])?></td>
+            <td><?= htmlspecialchars($row['deadline'])?></td>
+            <td><?= htmlspecialchars($row['status'])?></td>
+    </tr>
+    <?php endwhile; ?>
+    </table>
+    <?php else: ?>
+        <p>Tidak ada proyek yang terdaftar.</p>
+        <?php endif;?>
+
+        <br>
     <a href="../../includes/logout.php">Logout</a>
 </body>
 </html>
